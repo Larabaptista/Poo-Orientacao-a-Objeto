c12b046c421d5cf4f81bdb1c65bdb7f52fbb42af
Larabaptista <larinhabaptista21@gmail.com>
2024-03-25 19:32:41 -0300

    atualizado25-03

---
+++ ConstrutorAluno/Aluno.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConstrutorAluno
+{
+    public class Aluno
+    {
+        //.*.*.*.* TRABALHO, VALE 1 PONTO *.*.*.*.
+
+        //Data de entrega 31/03, pelo Teams, arquivo em .ZIP
+
+        //Criar os atributos matrícula, nome, p1, p2, media
+        //Criar os métodos/propriedade get e set, exceto o
+        //atributo média
+        //Criar o método para calcular a média do aluno
+        //O atributo matrícula não terá o método set, pois
+        //será gerado por um atributo static e armazenado
+        //no atributo matricula 
+        //Não use vetor
+        //Crie o método MostrarAtributos, chame-o após cada
+        //instância
+        //Instancia no mínimo 4 objetos
+    }
+}
